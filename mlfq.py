d9e1eb59b846dde1ddf1ac4c65c289fd757aff7d
lemonjerome <gabrielcatadmanramos@gmail.com>
2024-12-19 11:05:12 +0800

    feat: add mlfq file and group details

---
+++ mlfq.py
@@ -0,0 +1,13 @@
+# Group Members
+# Keanu Christopher Abriol | 202104256 | THR/FWX
+# Enrico Baratang | 202102378 | THY/FUV
+# Cedric John De Vera | 202212544 | THY/FUV
+# Gabriel Ramos | 202205080 | THY/FQR 
+
+##### IMPORTS #####
+
+##### CLASSES #####
+
+##### FUNCTIONS #####
+
+##### MAIN #####
